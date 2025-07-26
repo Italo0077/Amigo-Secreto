e7ebfd0afe9f87fb1d9dc44b3f8a73dbe9395576
Italo <italof363@gmail.com>
2025-07-26 14:14:15 -0300

    Adicionado função atualiza com li

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let amigos =[];
+let li = document.createElement('li');
+let listaAmigos = document.getElementById('listaAmigos');
 
 function adicionarAmigo() {
    let nome = document.querySelector('input').value;
@@ -6,8 +8,19 @@ function adicionarAmigo() {
     if (nome.length == '') {
         alert('Por favor insira um nome');
     }else{
+        amigos=[];
         amigos.push(nome);
+        atualizaListaAmigos(amigos);
         nome = document.querySelector('input').value = '';
-        console.log(amigos);
     }
+}
+
+function atualizaListaAmigos(amigos) {   
+    amigos.forEach(element => {
+        const li = document.createElement('li');
+        li.textContent = element;
+        listaAmigos.appendChild(li);
+        console.log(amigos);
+    });
+    
 }
